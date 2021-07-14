7d9ae7a245be4a5025f0a859668cfd5ff84966aa
ilhamsa1 <muhammad.ilhamsa1@gmail.com>
2021-07-14 21:56:33 +0700

    init commit

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+const { deleteNoteByIdHandler } = require('./api/notes/handler');
+const { editNoteByIdHandler } = require('./api/notes/handler');
+const { getNoteByIdHandler } = require('./api/notes/handler');
+const { addNoteHandler, getAllNotesHandler } = require('./api/notes/handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
