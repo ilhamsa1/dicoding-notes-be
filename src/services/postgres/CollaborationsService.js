17225636de0161cfd099b6c0a0838f627c07a9f4
ilhamsa1 <muhammad.ilhamsa1@gmail.com>
2021-10-17 17:40:51 +0700

    server-side caching implemented

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -4,8 +4,10 @@ const InvariantError = require('../../exceptions/InvariantError');
 const { nanoid } = require('nanoid');
 
 class CollaborationsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+
+    this._cacheService = cacheService;
   }
 
   async addCollaboration(noteId, userId) {
@@ -19,6 +21,9 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
+
+    await this._cacheService.delete(`notes:${userId}`);
+
     return result.rows[0].id;
   }
 
@@ -31,6 +36,7 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal dihapus');
     }
+    await this._cacheService.delete(`notes:${userId}`);
   }
 
   async verifyCollaborator(noteId, userId) {
